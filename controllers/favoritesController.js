9c090ebd061139d36d4109330cf584ba44bd0dd7
GA35a <ga35a35@email.com>
2025-06-28 11:38:54 +0300

    done favoritesController and favoriteRouter

---
+++ controllers/favoritesController.js
@@ -0,0 +1,43 @@
+const Favorite = require('../models/Favorite');
+const User = require('../models/Users')
+
+// Get all favorite movies for the logged-in user
+const getFavorites = async (req, res) => {
+  try {
+    const favorites = await Favorite.find({ userId: req.user.id }).populate('movieId');
+    res.send(favorites)
+  } catch (error) {
+    console.error('Error getting favorites:', error.message)
+  }
+}
+
+// Add a movie to favorites
+const addFavorite = async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userId);
+    const favorite = await Favorite.create(req.body);
+    res.send(favorite)
+  } catch (error) {
+    console.error('Failed to add favorite!', error.message)
+  }
+}
+
+// Remove a movie from favorites
+const removeFavorite = async (req, res) => {
+  try {
+    await Favorite.findByIdAndDelete(req.params.movieId);
+
+    res.send(`Favorite with ID ${req.params.movieId} has been deleted successfully!`)
+
+
+  } catch (error) {
+    console.error('An error has occurred deleting a favorite!', error.message)
+  }
+}
+
+
+module.exports = {
+  getFavorites,
+  addFavorite,
+  removeFavorite
+}
