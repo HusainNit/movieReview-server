e3eb518aa26fee24f55c09e5408dd40a8aab10b1
HusainNit <vaintime21@gmail.com>
2025-06-30 19:20:42 +0300

    some edits

--- models/Movie.js
+++ models/Movie.js
@@ -1,17 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const movieSchema = new mongoose.Schema(
-    { 
-        movieId: { type: String, required: true, unique: true },
-        title: { type: String, required: true },
-        genre: { type: String },
-        poster: { type: String },
-        releaseDate: { type: String },
-        description: { type: String }
-
-    },
-    {
-        timestamps: true
-    }
+  {
+    movieId: { type: String, required: true, unique: true },
+    // title: { type: String, required: true },
+    // genre: { type: String },
+    // poster: { type: String },
+    // releaseDate: { type: String },
+    // description: { type: String }
+  },
+  {
+    timestamps: true,
+  }
 );
-const Movie = mongoose.model('Movie',movieSchema);
+const Movie = mongoose.model("Movie", movieSchema);
 module.exports = Movie;
